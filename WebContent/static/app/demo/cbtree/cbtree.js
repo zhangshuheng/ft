3cb110898a861b4051e3297044f9ea1810745c9c
zhangshuheng <420828308@qq.com>
2013-01-14 21:43:26 +0800

    getCheckedNode

--- WebContent/static/app/demo/cbtree/cbtree.js
+++ WebContent/static/app/demo/cbtree/cbtree.js
@@ -1,26 +1,36 @@
 require([ "dojo/_base/connect", "dojo/data/ItemFileWriteStore",
-		"dojo/domReady", "cbtree/Tree","dojo/store/JsonRest",
-		"cbtree/models/ForestStoreModel" ],
-function(connect, ItemFileWriteStore, domReady, Tree,JsonRest, ForestStoreModel) {
+		"dojo/domReady", "cbtree/Tree", "dojo/store/JsonRest",
+		"cbtree/models/ForestStoreModel" ], function(connect,
+		ItemFileWriteStore, domReady, Tree, JsonRest, ForestStoreModel) {
 	function checkBoxClicked(item, nodeWidget, evt) {
-//		var len = store.each
-		console.log(store);
-//		alert("The new state for " + this.getLabel(item) + " is: "
-//				+ nodeWidget.get("checked"));
+		// Define a callback that fires when all the items are returned.
+		var gotList = function(items, request) {
+			dojo.forEach(items, function(item) {
+				if (item.checked[0]) {
+					console.log(item);
+				}
+			});
+		}
+		var gotError = function(error, request) {
+			alert("The request to the store failed. " + error);
+		}
+		// Invoke the search
+		store.fetch({
+			query : {
+				checked : true
+			},
+			onComplete : gotList,
+			onError : gotError
+		});
 	}
 	var store = new ItemFileWriteStore({
-		url : contextPath+"/demo/treejson.do"
+		url : contextPath + "/demo/treejson.do"
 	});
-//	var store = new JsonRest({
-//		idProperty:'name',
-//	    target: contextPath+"/demo/treejson.do",
-//	    sortParam: "sortBy"
-//	});
-	
+
 	var model = new ForestStoreModel({
 		store : store,
 		query : {
-			type : 'parent'
+			type : 2
 		},
 		rootLabel : 'The Family'
 	});
@@ -32,6 +42,7 @@ function(connect, ItemFileWriteStore, domReady, Tree,JsonRest, ForestStoreModel)
 		checkBoxes : true,
 		nodeIcons : true
 	});
+
 	connect.connect(tree, "onCheckBoxClick", model, checkBoxClicked);
 	domReady(function() {
 		tree.placeAt('CheckboxTree');
